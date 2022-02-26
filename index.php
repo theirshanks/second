84e1657dd19da14864e582852890d7c7f300fca1
theirshanks <100458554+theirshanks@users.noreply.github.com>
2022-02-26 12:10:27 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,27 @@
   <input name="uploadedfile" type="file" value="choose">
   <input type="submit" value="Upload">
 </form>
+$draw = new \ImagickDraw();
+$draw->setStrokeColor('Green');
+$draw->setFillColor('Red');
+$draw->setStrokeWidth(7);
+$draw->rectangle(40, 30, 200, 260);
+  
+// Create an image object which the draw
+// commands can be rendered into
+$image = new \Imagick();
+$image->newImage(300, 300, 'White');
+$image->setImageFormat("png");
+  
+// Render the draw commands in the ImagickDraw object 
+// into the image.
+      
+$image->drawImage($draw);
+  
+// Send the image to the browser
+header("Content-Type: image/png");
+echo $image->getImageBlob();
+
 <?php
 if ( isset($_FILES['uploadedfile']) ) {
  $filename  = $_FILES['uploadedfile']['tmp_name'];
